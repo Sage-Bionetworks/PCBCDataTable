9ebef60d137bd20ad61b93687e4c4548c51ff9ea
kdaily <kdaily@users.noreply.github.com>
2015-04-28 13:07:21 -0700

    changed assignment operator

--- server.R
+++ server.R
@@ -3,28 +3,28 @@ library(DT)
 
 shinyServer(function(input, output) {
   
-  output$mrna = DT::renderDataTable({
+  output$mrna <- DT::renderDataTable({
     DT::datatable(mrna, 
                   extensions = c('ColVis'),
                   options = list(pageLength = 15),
                   escape=1)
     })
 
-  output$mirna = DT::renderDataTable({
+  output$mirna <- DT::renderDataTable({
     DT::datatable(mirna, 
                   extensions = c('ColVis'),
                   options = list(pageLength = 15),
                   escape=1)
   })
   
-  output$methylation = DT::renderDataTable({
+  output$methylation <- DT::renderDataTable({
     DT::datatable(methylation, 
                   extensions = c('ColVis'),
                   options = list(pageLength = 15),
                   escape=1)
   })
   
-  output$all = DT::renderDataTable({
+  output$all <- DT::renderDataTable({
     DT::datatable(allData, 
                   extensions = c('ColVis'),
                   options = list(pageLength = 15),
