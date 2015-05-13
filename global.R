a258cbf690d79146a51e8ead97c01ae875334c9c
kdaily <kdaily@users.noreply.github.com>
2015-05-13 10:45:42 -0700

    added some docs, simplified queries

--- global.R
+++ global.R
@@ -6,21 +6,34 @@ library(synapseClient)
 
 synapseLogin()
 
+# Which columns to consider
 colsToUse <- c('id', 'dataType', 'fileType', 'fileSubType', 'UID', 'biologicalSampleName', 
                'C4_Cell_Line_ID', 'Originating_Lab_ID', 'Originating_Lab', 'Cell_Line_Type',
                'Cell_Type_of_Origin', 'Tissue_of_Origin', 'Reprogramming_Vector_Type',
                'Reprogramming_Gene_Combination', 'pass_qc', 'exclude')
 
+# query the table with specific columns
 colsToUseStr <- paste(colsToUse, collapse=",")
-rnaData <- synQuery(sprintf("select %s from file where benefactorId=='syn1773109' and dataType=='%s'", colsToUseStr, 'mRNA'), blockSize=400)$collectAll()
-mirnaData <- synQuery(sprintf("select %s from file where benefactorId=='syn1773109' and dataType=='%s'", colsToUseStr, 'miRNA'), blockSize=400)$collectAll()
-methylationData <- synQuery(sprintf("select %s from file where benefactorId=='syn1773109' and dataType=='%s'", colsToUseStr, 'methylation'), blockSize=400)$collectAll()
+queryTemplate <- "select %s from file where benefactorId=='syn1773109' and dataType=='%s'"
 
+rnaData <- synQuery(sprintf(queryTemplate, colsToUseStr, 'mRNA'), 
+                    blockSize=400)$collectAll()
+
+mirnaData <- synQuery(sprintf(queryTemplate, colsToUseStr, 'miRNA'), 
+                      blockSize=400)$collectAll()
+
+methylationData <- synQuery(sprintf(queryTemplate, colsToUseStr, 'methylation'), 
+                            blockSize=400)$collectAll()
+
+# Combine the data together
 allData <- rbind(rnaData, mirnaData, methylationData)
 colnames(allData) <- gsub(".*\\.", "", colnames(allData))
 allData <- allData[, colsToUse]
+
+# Turn IDs into urls that open in new tab/window
 allData$id <- paste('<a href="https://www.synapse.org/#!Synapse:', allData$id, '" target="_blank">', allData$id, '</a>', sep="")
 
+# Filtering for specific data types and making the data for specific tabs
 mrna <- allData %>% 
   filter(dataType=='mRNA') %>%
   filter(!is.na(UID),
