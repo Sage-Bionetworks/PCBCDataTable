a38a64926662f0eaeae99562f0d64abfd02280b6
kdaily <kdaily@users.noreply.github.com>
2015-10-29 10:20:08 -0700

    allow column filtering

--- server.R
+++ server.R
@@ -4,31 +4,39 @@ library(DT)
 shinyServer(function(input, output) {
   
   output$mrna <- DT::renderDataTable({
-    DT::datatable(mrna2, 
+    DT::datatable(mrna2,
+                  filter = list(position = 'top', clear = FALSE),
                   options = list(pageLength = 15,
-                                 dom='frtip'),
+                                 dom='frtip',
+                                 search = list(regex = TRUE)),
                   escape=1)
     })
 
   output$mirna <- DT::renderDataTable({
     DT::datatable(mirna2, 
+                  filter = list(position = 'top', clear = FALSE),
                   options = list(pageLength = 15,
-                                 dom='frtip'),
+                                 dom='frtip',
+                                 search = list(regex = TRUE)),
                   escape=1)
   })
   
   output$methylation <- DT::renderDataTable({
     DT::datatable(methylation,
+                  filter = list(position = 'top', clear = FALSE),
                   options = list(pageLength = 15,
-                                 dom='frtip'),
+                                 dom='frtip',
+                                 search = list(regex = TRUE)),
                   escape=1)
   })
   
   output$all <- DT::renderDataTable({
     DT::datatable(allData, 
                   extensions = c('ColReorder', 'ColVis'),
+                  filter = list(position = 'top', clear = FALSE),
                   options = list(pageLength = 15,
-                                 dom='C<"clear">Rfrtip'),
+                                 dom='C<"clear">Rfrtip',
+                                 search = list(regex = TRUE)),
                   escape=1)
   })
 
